2e143fd18f3094b5842c778294d7f333d889bbf0
suresh <sureshc.behera@gmail.com>
2024-12-18 00:25:49 +0530

    add

---
+++ src/main/java/com/valuelabs/trackingnumber/TrackingNumberApplication.java
@@ -0,0 +1,14 @@
+package com.valuelabs.trackingnumber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackingNumberApplication {
+
+    public static void main(String[] args) {
+        System.out.println("hi");
+        SpringApplication.run(TrackingNumberApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/valuelabs/trackingnumber/TrackingNumberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valuelabs.trackingnumber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackingNumberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
